605c5135054d59743d6cd54492942ab1b4df0917
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2020-06-21 16:19:53 +0800

    完成

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+AOS.init();  //初始化
 $('#banner').slick({
     autoplay:true,
     autoplaySpeed:1500,
-    arrows:false
+    arrows:false //輪播片的左右箭頭 false為去除
 });
 $('#brand').slick({
     slidesToShow:4,
     autoplay:true,
     autoplaySpeed:1000,
-    dots:true,
+    dots:true, //圖片下方的點點
     arrows:false
 });
